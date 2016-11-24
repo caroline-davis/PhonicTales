22a349450bf5b950588423e424544d73b9cd1aed
Caroline Davis <caroline_davis@live.com>
2016-11-24 14:35:11 +1100

    feat: Add cocoapods for fb login

--- PhonicTales/LoginViewController.swift
+++ PhonicTales/LoginViewController.swift
@@ -8,10 +8,28 @@
 
 import UIKit
 
+import FacebookCore
+import FacebookLogin
+
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let accessToken = AccessToken.current {
+            // User is logged in, use 'accessToken' here.
+            
+            self.dismiss(animated: true, completion: {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController")
+                self.present(vc!, animated: true, completion: nil)
+            })
+            
+            
+        } else {
+            let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+            loginButton.center = view.center
+            
+            view.addSubview(loginButton)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
